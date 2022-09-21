20c1278f662c4a238bdf9106d465bd311d5d5234
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-21 12:51:34 +0200

    index.js init

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './main.css';
 
+let szinek = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'cyan',
+    'blue',
+    'purple',
+];
 
+function szinvaltas() {
+    console.log('katt');
+}
+
+function init() {
+    document.getElementById('szinvalto').addEventListener('click',szinvaltas);
+}
+
+document.addEventListener('DOMContentLoaded',init);
 
 console.log('Loaded');
